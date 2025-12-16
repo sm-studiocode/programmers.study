faae1da322dbfc9d353927b56748c71c628ff93f
sm-studiocode <sm.codes.coding@gmail.com>
2025-12-16 19:58:48 +0900

    [level 0] Title: 배열 만들기 4, Time: 20.82 ms, Memory: 96.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i = 0; i<arr.length; i++){
+            if(list.isEmpty()){
+                list.add(arr[i]);
+                continue;
+            }else{
+                if(list.get(list.size()-1)<arr[i]){
+                    list.add(arr[i]);
+                }else{
+                    list.remove(list.size()-1);
+                    i--;
+                }
+            }
+        }
+        
+        int[] stk = new int[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            stk[i] = list.get(i);
+        }
+        return stk;
+    }
+}
