21c4ca1213803bc679882ba3e6796d2fa71d8dd3
sm-studiocode <sm.codes.coding@gmail.com>
2025-12-29 19:37:24 +0900

    [level 0] Title: 접미사 배열, Time: 0.42 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        int len = my_string.length();
+        String[] answer = new String[len];
+
+        for(int i=0; i<len; i++){
+            answer[i]=my_string.substring(i);
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+}
