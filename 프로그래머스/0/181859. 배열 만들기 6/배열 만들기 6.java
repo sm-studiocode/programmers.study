15aee5a6bdf04bae80a12d08d3a443f466ea43c3
sm-studiocode <sm.codes.coding@gmail.com>
2026-01-18 22:02:41 +0900

    [level 0] Title: 배열 만들기 6, Time: 42.65 ms, Memory: 148 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr) {
+        List<Integer> stk = new ArrayList<>();
+        
+        for(int i=0; i<arr.length; i++){
+            if(stk.isEmpty()){
+                stk.add(arr[i]);
+            }else if(!stk.isEmpty()){
+                int num = stk.get(stk.size()-1);
+                if(num == arr[i]){
+                    stk.remove(stk.size()-1);
+                }else{
+                    stk.add(arr[i]);
+                }
+            }
+        }
+        
+        if(stk.isEmpty()){
+            stk.add(-1);
+        }
+        int[] answer = new int[stk.size()];
+        
+        for(int i=0; i<stk.size(); i++){
+            answer[i] = stk.get(i);
+        }
+        return answer;
+    }
+}
